1e674e8c9227f9fabca20c8c7ee1f855d8a00f40
ajnash-credot <108794235+ajnash-credot@users.noreply.github.com>
2024-04-19 22:37:59 +0530

    middleware feature implementation

--- src/body-parser.ts
+++ src/body-parser.ts
@@ -1,15 +1,23 @@
 import type http from "http";
 import bp from "body-parser";
 import { IncomingMessage, ServerResponse } from "http";
+import { Context } from "./types";
 const { json, urlencoded } = bp;
 
 const _bodyParserUrl = urlencoded({ extended: true });
 const _bodyParserJon = json();
 
-export default function (req: http.IncomingMessage, res: ServerResponse<IncomingMessage>,next:()=>void) {
-   
-    _bodyParserUrl(req, res, () => {
-        _bodyParserJon(req, res, () => next());
-      });
+// export default () => {
+//   return ({ req, res }: Context, next: () => void) => {
+//     console.log("in bodyparser")
+//     _bodyParserUrl(req, res, () => {
+//       _bodyParserJon(req, res, () => next());
+//     });
+//   };
+// };
 
-}
+export default (req: any, res: any, next: () => void) => {
+  _bodyParserUrl(req, res, () => {
+    _bodyParserJon(req, res, () => next());
+  });
+};
--- test/client.js
+++ test/client.js
@@ -12,11 +12,25 @@ const s = new app({
 s.listen(8001, () => {
   console.log("port running");
 });
-s.get("/test", ({ req, res }) => {
+s.use(({req,res},next)=>{
+  console.log("it is middleware");
+  next()
+})
+s.use(({req,res},next)=>{
   
-  res.text("hai")
+  console.log("it is middleware2",req.body);
+ next() 
+})
+
+s.use(({req,res},next)=>{
+  console.log("it is middleware3");
+ next() 
+})
+s.get("/test", ({ req, res }) => {
+  console.log("this is router")
+  res.text("2")
 });
-s.get("/query", ({ req, res }) => {
-  console.log({ query: req.query });
-  res.end("haiii");
+s.post("/query", ({ req, res }) => {
+  console.log("bodyyy",req.body)
+  res.json(req.body)
 });
