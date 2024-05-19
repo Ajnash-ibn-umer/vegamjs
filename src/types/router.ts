bdbcb227ab3665668504f70e3c055c280d816db4
ajnash-credot <108794235+ajnash-credot@users.noreply.github.com>
2024-05-19 19:59:14 +0530

    fix

--- src/types/router.ts
+++ src/types/router.ts
@@ -3,6 +3,6 @@ import { PluginHandler } from '.';
 import fileUpload from 'express-fileupload';
 export type AppConfig={
 routerOptions?:findMyWay.Config<findMyWay.HTTPVersion.V1>,
-plugins:PluginHandler[] ,
-fileUploadOptions:fileUpload.Options
+plugins?:PluginHandler[] ,
+fileUploadOptions?:fileUpload.Options
 }
